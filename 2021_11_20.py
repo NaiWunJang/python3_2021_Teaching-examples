33156aa41a4595368828ebb5f4eb0de555745534
WillJang <willjang@protonmail.com>
2021-11-20 19:11:19 +0800

    Second edit

--- 2021_11_20.py
+++ 2021_11_20.py
@@ -1,5 +1,15 @@
 ############################################################
 ################ Tuples ####################################
 ############################################################
-coordinates = (4,5)
+coordinates = (1,2)
+#coordinates = [1] = 10
+#When coordinates = (4,5) is wrong
+#() is list;[] is Tuples !!!
+print(coordinates[1])
+coordinates =[1,2]
+coordinates[1] = 10
+print(coordinates[1])
+coordinates =[(1,2),(3,4),(5,6)]
+print(coordinates[1])
+coordinates[1] = 10
 print(coordinates[1])
